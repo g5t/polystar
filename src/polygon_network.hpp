9b881ee05c1e9b301b008a537b35ba25a84fe81a
Gregory Tucker <gregory.tucker@gmail.com>
2023-02-13 18:07:40 +0100

    Stash

--- src/polygon_network.hpp
+++ src/polygon_network.hpp
@@ -15,6 +15,38 @@ namespace polystar::polygon {
   template<class T, template<class> class A> class Poly;
   Wire wire_merge(const Wire &, const Wire &);
 
+    template<class W>
+  class NetworkCost {
+  public:
+      using wire_t = std::shared_ptr<W>;
+      using cost_t = std::pair<double, double>;
+      using path_t = std::pair<std::vector<ind_t>, std::vector<ind_t>>;
+      using elem_t = std::pair<cost_t, path_t>;
+      using map_t = std::map<wire_t, elem_t>;
+  private:
+      map_t map_;
+  public:
+      bool known(wire_t p) const {
+          return std::find(map_.begin(), map_.end(), [p](const auto & x){
+              const auto & [w, z] = x;
+              return p == w;
+          }) != map_.end();
+      }
+      elem_t cost(wire_t p) const {
+          auto ptr = std::find(map_.begin(), map_.end(), [p](const auto & x){
+              const auto & [w, z] = x;
+              return p == w;
+          });
+          if (ptr != map_.end()) {
+              const auto & [w, z] = *ptr;
+              return z;
+          }
+          return std::make_pair(std::make_pair<double, double>(-1, -1), std::make_pair(std::vector<ind_t>(), std::vector<ind_t>()));
+      }
+      const elem_t & operator[](const wire_t & w) const {return map_[w];}
+      elem_t & operator[](const wire_t & w) {return map_[w];}
+  };
+
   template<class T, template<class> class A>
   class Network {
   public:
@@ -41,9 +73,9 @@ namespace polystar::polygon {
       }
     }
 
-    size_t size() const { return map_.size(); }
+    [[nodiscard]] size_t size() const { return map_.size(); }
 
-    std::vector<Wire> wires() const {
+    [[nodiscard]] std::vector<Wire> wires() const {
       std::vector<Wire> w;
       w.reserve(size());
       for (const auto & [x, l]: map_) w.emplace_back(*(x.get()));
@@ -73,7 +105,50 @@ namespace polystar::polygon {
       return old_size - map_.size();
     }
 
+
+    std::optional<wire_t> containing_wire(const vertex_t & x);
+
     bool simplify();
+
+    bool search_path(NetworkCost<wire_t, T, A> & costs, double minimum, double maximum, std::vector<wire_t>&  border, wire_t goal, A<T> short_path, A<T> long_path, const A<T>& in_first, const A<T>& in_last){
+        while (!border.empty()){
+            // find the cheapest border polygon
+            std::partial_sort(border.begin(), border.begin()+1, border.end(),
+                              [&](const auto & a, const auto & b) -> bool {
+                auto a_cost = costs[a];
+                auto b_cost = costs[b];
+                if (a_cost.first.second <= b_cost.first.first) return true;
+                if (a_cost.first.first >= b_cost.first.second) return false;
+                return a_cost.first.first < b_cost.first.first;
+            });
+            auto cbp = border.front();
+            // remove the cheapest from the border
+            border.erase(border.begin());
+
+            // get the connected polygons from the cheapest
+            auto next = map_[cbp];
+            // by definition we can reach all connected polygons, but we do not want to backtrack
+            next.erase(std::remove_if(next.begin(), next.end(),
+                                      [&](const auto & np){return costs.known(np);}), next.end());
+
+            // grab the cost information up to the current location
+            auto cbp_cost = costs[cbp];
+            // now decide which of next should be visited
+            for (auto & n: next){
+                // find the connecting edge between where the cheapest border and this next polygon
+                auto edge = common_edge(cbp, n);
+                auto e0 = vertices_.view(edge.first);
+                auto e1 = vertices_.view(edge.second);
+                // figure out the range of costs to move to the extremes of the edge.
+                if (cbp_cost.second.first.empty() || cbp_cost.second.second.empty()){
+                    // we're in the first polygon -- calculate from in_first to the edge points
+                    
+                }
+
+            }
+
+        }
+    }
 
     vertex_t path(const vertex_t & from, const vertex_t & to) const {
       /* 1. Find the ending poylygon
@@ -81,12 +156,36 @@ namespace polystar::polygon {
        * 3. Remove extraneous points on the path
        *    (if the path is A-B-C-..., and A-C intersects the edge containing B, B is extraneous
        */
-      auto out = from.decouple();
+      auto first = containing_wire(to);
+      auto last = containing_wire(from);
+      if (!first.has_value() || !last.has_value()) return from;
+
+      NetworkCost<wire_t, T, A> costs;
+      std::vector<wire_t> border{first};
+      auto success = search_path(costs, 0, 0, border, last, from.decouple(), from.decouple(), to, from);
+
+
       // add all the intervening points
       // add the final point:
       out = cat(0, out, to);
       return out;
     }
+  private:
+      std::pair<ind_t, ind_t> common_edge(const wire_t & asp, const wire_t & bsp){
+          // error checking to ensure that they're in each others' connected lists? No, since this is private
+          auto a = asp.get();
+          auto b = bsp.get();
+          auto as = static_cast<ind_t>(a->size());
+          auto bs = static_cast<ind_t>(b->size());
+          for (ind_t i=0; i<as; ++i){
+              for (ind_t j=0; j<bs; ++j){
+                  if (a->operator[](i)  == b->operator[]((j+1)%bs) && b->operator[](j) == a->operator[]((i+1)%as)) {
+                      return std::make_pair(a->operator[](i), b->operator[](j));
+                  }
+              }
+          }
+          return std::make_pair(0, 0);
+      }
   };
 }
 
--- src/polygon_network.tpp
+++ src/polygon_network.tpp
@@ -1,6 +1,43 @@
 #ifndef POLYSTAR_POLYGON_NETWORK_TPP
 #define POLYSTAR_POLYGON_NETWORK_TPP
 
+static
+std::tuple<std::array<polystar::ind_t,3>, std::array<polystar::ind_t,3>>
+merged_corners(const polystar::polygon::Wire & a, const polystar::polygon::Wire & b) {
+    using namespace polystar;
+    using c_t = std::array<ind_t, 3>;
+    auto as = static_cast<ind_t>(a.size());
+    auto bs = static_cast<ind_t>(b.size());
+    for (ind_t a1=0; a1<as; ++a1){
+        auto a0 = (a1 + as - 1) % as;
+        auto a2 = (a1 + 1) % as;
+        auto a3 = (a1 + 2) % as;
+        for (ind_t b1=0; b1<bs; ++b1){
+            auto b0 = (b1 + bs - 1) % bs;
+            auto b2 = (b1 + 1) % bs;
+            auto b3 = (b1 + 2) % bs;
+            if (a1 == b2 && a2 == b1){
+                c_t va{{a0, a1, b3}};
+                c_t vb{{b0, b1, a3}};
+                return std::make_tuple(va, vb);
+            }
+        }
+    }
+    return std::make_tuple(c_t(), c_t());
+}
+
+template<class T, template<class> class A>
+bool would_be_convex(const polystar::polygon::Wire & a, const polystar::polygon::Wire & b, const A<T>& x){
+  // We already know the neighbours share an edge, otherwise they would not be neighbours.
+  // So find the edge, then check for convex-ness of the new corners
+  auto [va, vb] = merged_corners(a, b);
+  // we've thus far required that every polygon in the network is convex -- we exploit that now
+  auto is_convex = [&](const auto & g){
+      return cross2d(x.view(g[1]) - x.view(g[0]), x.view(g[2]) - x.view(g[1])).val(0,0) > 0;
+  };
+  return is_convex(va) && is_convex(vb);
+}
+
 template<class T, template<class> class A>
 bool polystar::polygon::Network<T,A>::simplify() {
   list_t merged_away;
@@ -17,29 +54,26 @@ bool polystar::polygon::Network<T,A>::simplify() {
     // skip this one if it has already been merged into another
     if (is_in(merged_away, w)) continue;
     // try combining this wire with its connected wires
-    for (const auto & t: l){
-      if (is_in(merged_away, t)) continue;
-      auto test = polystar::polygon::wire_merge(*(w.get()), *(t.get()));
-      if (test.is_convex(vertices_)){
-        // We can combine the wires -- which requires combining their connected lists too
-        auto tl = map_[t];
-        std::copy_if(tl.begin(), tl.end(), std::back_inserter(l), [&](const auto & x){return !is_in(l, x);});
-        // Keep pointers to the now-merged wires, so we don't attempt to merge them again
-        merged_away.push_back(w);
-        merged_away.push_back(t);
-        // go through the connections to this wire *and* that wire, remove pointers to the pre-merged wires
-        auto new_w = std::make_shared<Wire>(test);
-        std::for_each(l.begin(), l.end(), [&](const auto & x){remove_and_replace(x, w, new_w);});
-        std::for_each(tl.begin(), tl.end(), [&](const auto & x){remove_and_replace(x, t, new_w);});
-        // remove the reference to the other wire from this list
-        l.erase(std::remove_if(l.begin(), l.end(), [&](const auto & x){return x==t;}), l.end());
-        // remove the other wire and its reference list from the map
-        erase_wire(t);
-        // add the new wire with the merged result
-        map_[new_w] = l;
-        // remove the current pre-merged wire
-        map_.erase(ptr);
-      }
+    for (const auto & t: l) if (!is_in(merged_away, t)) if (would_be_convex(*(w.get()), *(t.get()), vertices_)){
+      auto merged = polystar::polygon::wire_merge(*(w.get()), *(t.get()));
+      // We can combine the wires -- which requires combining their connected lists too
+      auto tl = map_[t];
+      std::copy_if(tl.begin(), tl.end(), std::back_inserter(l), [&](const auto & x){return !is_in(l, x);});
+      // Keep pointers to the now-merged wires, so we don't attempt to merge them again
+      merged_away.push_back(w);
+      merged_away.push_back(t);
+      // go through the connections to this wire *and* that wire, remove pointers to the pre-merged wires
+      auto new_w = std::make_shared<Wire>(merged);
+      std::for_each(l.begin(), l.end(), [&](const auto & x){remove_and_replace(x, w, new_w);});
+      std::for_each(tl.begin(), tl.end(), [&](const auto & x){remove_and_replace(x, t, new_w);});
+      // remove the reference to the other wire from this list
+      l.erase(std::remove_if(l.begin(), l.end(), [&](const auto & x){return x==t;}), l.end());
+      // remove the other wire and its reference list from the map
+      erase_wire(t);
+      // add the new wire with the merged result
+      map_[new_w] = l;
+      // remove the current pre-merged wire
+      map_.erase(ptr);
     }
   }
 //      for (auto & [w, l] : map_){
@@ -71,6 +105,15 @@ bool polystar::polygon::Network<T,A>::simplify() {
 //        }
 //      }
   return !merged_away.empty();
+}
+
+template<class T, template<class> class A>
+std::optional<typename polystar::polygon::Network<T,A>::wire_t> polystar::polygon::Network<T,A>::containing_wire(const typename polystar::polygon::Network<T,A>::vertex_t & x){
+    for (const auto & [w, l]: map_) {
+        auto x_in = w.get()->contains(x, vertices_, false);
+        if (std::count(x_in.begin(), x_in.end(), true) == static_cast<size_t>(x.size(0))) return std::make_optional(w);
+    }
+    return std::nullopt;
 }
 
 #endif
